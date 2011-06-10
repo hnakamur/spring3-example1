7bb45dbf8e4eeea0873d1187c626544d9994c9ec
Hiroaki Nakamura <hnakamur@gmail.com>
2011-06-11 01:50:47 +0900

    Hello world application using spring framework 3.0.

---
+++ src/main/java/example1/controller/HelloController.java
@@ -0,0 +1,14 @@
+package example1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/hello")
+public class HelloController {
+	@RequestMapping(method=RequestMethod.GET)
+	public String doGet() {
+		return "hello";
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello Spring World!</h2>
+</body>
+</html>
